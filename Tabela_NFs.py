c18760d32e62eb0d238d374f3c8aaf640a9a28ed
MauricioAlouan1 <mauricio.alouan@gmail.com>
2025-06-16 13:27:06 -0300

    up to date

--- Tabela_NFs.py
+++ Tabela_NFs.py
@@ -6,7 +6,7 @@ from openpyxl import load_workbook
 
 # Run processing for all series for a specific month
 YEAR = "2025"
-MONTH = "3-Março"
+MONTH = "5-Maio"
 
 # Define base folder and available series
 BASE_FOLDER = "/Users/mauricioalouan/Dropbox/nfs"
