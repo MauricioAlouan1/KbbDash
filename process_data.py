1bbb4039fae1a0e0f4c630e895b1d01f05efac59
MauricioAlouan1 <mauricio.alouan@gmail.com>
2025-09-15 08:55:57 -0300

    fixed space after bling codigo

--- process_data.py
+++ process_data.py
@@ -575,8 +575,8 @@ def check_and_process_files():
         'O_CC': (process_O_CC, "Situação", False),
         'O_CtasAPagar': (process_O_CtasAPagar, "Minha Empresa (Nome Fantasia)", False),
         'O_CtasARec': (process_O_CtasAPagar, "Minha Empresa (Nome Fantasia)", False),
-        'B_Estoq': (process_B_Estoq, "Código", False),
-        'B_EFull': (process_B_Estoq, "Código", False),
+        'B_Estoq': (process_B_Estoq, "Código ", False),
+        'B_EFull': (process_B_Estoq, "Código ", False),
         'L_LPI': (process_L_LPI, "Data", False),
         'O_Estoq': (process_O_Estoq, "Código do Produto", False),
         'MLK_Vendas': (process_MLK_Vendas, "N.º de venda", True),  # Enable hyperlink extraction for MLK_Vendas
